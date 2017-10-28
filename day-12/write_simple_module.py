88486ff5c1deb898934ac4ac9c97a8e250babd9d
dreamapple <2451731631@qq.com>
2017-10-28 13:30:01 +0800

    add day-12

---
+++ day-12/write_simple_module.py
@@ -0,0 +1,18 @@
+'this is a test module'
+
+__author__ = 'dreamapple'
+
+import sys
+
+def test():
+    args = sys.argv
+    if len(args)==1:
+        print('Hello,World')
+    elif len(args)==2:
+        print('Hello, %s' % args[1])
+    else:
+        print('Too many arguments!')
+
+
+if __name__=='__main__':
+    test()
