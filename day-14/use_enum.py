92d959b9724532ff1d25d216b951f9ab99875491
dreamapple <2451731631@qq.com>
2017-12-20 19:43:34 +0800

    add enum

---
+++ day-14/use_enum.py
@@ -0,0 +1,26 @@
+from enum import Enum, unique
+
+Month = Enum('Month', ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
+
+for name, member in Month.__members__.items():
+    print(name, '=>', member, ',', member.value)
+
+@unique
+class Weekday(Enum):
+    Sun = 0
+    Mon = 1
+    Tue = 2
+    Wed = 3
+    Thu = 4
+    Fri = 5
+    Sat = 6
+
+for name, day in Weekday.__members__.items():
+    print(name, '=>', day, '=>', day.value)
+
+
+print(Weekday.Sun)
+print(Weekday.Sun.value)
+print(Weekday['Sun'])
+print(Weekday['Sun'].value)
+print(Weekday(0))
