d36bc5d85aaab5d42164ff51d340e85035321fd8
dreamapple <2451731631@qq.com>
2018-12-09 22:40:56 +0800

    第一章 done

---
+++ learn-auto/01/hello-world.py
@@ -0,0 +1,24 @@
+# myName = input('请输入你的姓名：')
+# print('你的姓名是： ' + myName)
+# # 类型转换 将整数转换为字符串 str(6)
+# print('你名字的长度是: ' + str(len(myName)))
+
+# 类型转换
+print(str(1))
+print(int('1'))
+print(float('1.0'))
+
+# 整数和浮点数的比较
+print(1 == 1.0)
+print(1 == '1')
+
+print('hello' * 2)
+print(27 / 4)
+print(27 // 4)
+print(3 * 3)
+print(3 ** 3)
+
+# round函数
+print(round(0.5))
+print(round(-0.5))
+print(round(2.675, 2))
